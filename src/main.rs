7a645878fcf951ae7abf76bb9db2274ff343713d
Topher <winwardo@gmail.com>
2019-07-11 14:38:58 +0100

    First failing test

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,6 @@ fn main() {}
 struct Game {}
 
 enum RollStatus {
-    First,
-    Second,
-    Third,
     Invalid,
 }
 
@@ -19,6 +16,7 @@ impl Game {
     fn roll(&mut self, pins: u8) -> RollStatus {
         RollStatus::Invalid
     }
+
     fn score(&self) -> u16 {
         0
     }
@@ -36,4 +34,19 @@ mod tests {
         let game = Game::new();
         assert_eq!(0, game.score());
     }
+
+    #[rstest]
+    fn first_roll_is_0_score_is_0() {
+        let mut game = Game::new();
+        game.roll(0);
+        assert_eq!(0, game.score());
+    }
+
+    #[rstest]
+    fn first_roll_is_9_score_is_9() {
+        let mut game = Game::new();
+        game.roll(9);
+        assert_eq!(9, game.score());
+    }
+
 }
